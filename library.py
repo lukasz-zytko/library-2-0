9367206977a32e308dcb7beeaa17936d54649179
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-02-06 20:20:44 +0100

    Add db configurator and object Book

--- library.py
+++ library.py
@@ -1,1 +1,9 @@
-from app import app
+from app import app, db
+from app.models_sqlalchemy import Book
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Book": Book
+    }
