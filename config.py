10f4606a73755393a61d360134acc76c4f2cab34
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-02-06 21:27:12 +0100

    Migrate first db and add few instances

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
    SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"
    SQLALCHEMY_DATABASE_URI = ( 
            os.environ.get('DATABASE_URL') or
-           'sqlite:///' + os.path.join(BASE_DIR, 'mikroblog.db')
+           'sqlite:///' + os.path.join(BASE_DIR, 'library.db')
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
