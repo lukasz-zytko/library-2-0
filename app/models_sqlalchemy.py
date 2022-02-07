fe09d7b2db6e5434a25b0658b7e4296c9b9860f9
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-02-07 22:01:53 +0100

    Add new classes to db

--- app/models_sqlalchemy.py
+++ app/models_sqlalchemy.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,7 +8,7 @@ class Author(db.Model):
     books = db.relationship("Book", backref="author", lazy="dynamic")
 
     def __str__(self) -> str:
-        return f"Autor {self.name}"
+        return f"Autor: {self.name}"
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +16,16 @@ class Book(db.Model):
     series = db.Column(db.String(300), index=True)
     pages = db.Column(db.Integer)
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
+    book = db.relationship("Borrow", backref="book", lazy="dynamic")
 
     def __str__(self) -> str:
-        return f"Book {self.title} {self.author}"
+        return f"Książka: {self.title}, {self.author}"
+
+class Borrow(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"))
+    status = db.Column(db.String(50), index=True, default="pożyczona")
+    date = db.Column(db.Date, default=datetime.utcnow)
+
+    def __str__(self) -> str:
+        return f"{self.book} status: {self.status}, data: {self.date}"
--- library.py
+++ library.py
@@ -1,10 +1,11 @@
 from app import app, db
-from app.models_sqlalchemy import Book, Author
+from app.models_sqlalchemy import Book, Author, Borrow
 
 @app.shell_context_processor
 def make_shell_context():
     return {
         "db": db,
         "Book": Book,
-        "Author": Author
+        "Author": Author,
+        "Borrow": Borrow
     }
