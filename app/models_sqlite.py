dc12f7ff6c0c4e472bb9bb58bc943ae7c89638f5
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-02-08 16:32:37 +0100

    Add corr

--- app/models_sqlite.py
+++ app/models_sqlite.py
@@ -2,19 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import Table, MetaData
 from sqlite3 import Error
 
-
-"""
-engine = create_engine('sqlite:///library.db')
-conn = engine.connect()
-
-table = Table("author", metadata, autoload=True, autoload_with=engine)
-meta = MetaData()
-result = conn.execute(table.select())
-
-for re in result:
-    print(re)
-"""
-engine = create_engine('sqlite:///library.db')
+engine = create_engine('sqlite:///library.db', connect_args={'check_same_thread': False})
 metadata = MetaData()
 
 
@@ -26,28 +14,27 @@ class SQLite:
         except Error as e:
             print(e)
 
-    def create_connection(self):
-        try:          
-            self.conn = engine.connect()
-        except Error as e:
-            print(e)
-        return self.conn
-
-    def get_table(self,table_name):        
-        table = Table(table_name, metadata, autoload=True, autoload_with=engine)
+    def get_table(self,table_name):
+        self.table_name = table_name
+        self.conn = engine.connect()    
+        table = Table(self.table_name, metadata, autoload=True, autoload_with=engine)
         with self.conn as conn:
             result = conn.execute(table.select()).fetchall()
         return result
     
     def add(self,table_name,data):
+        self.table_name = table_name
+        self.data = data
+        self.conn = engine.connect()
         data.pop("csrf_token")
-        table = Table(table_name, metadata, autoload=True, autoload_with=engine)
+        table = Table(self.table_name, metadata, autoload=True, autoload_with=engine)
         if table_name == "book":
             ins = table.insert().values(title=data["title"], series = data["series"], pages = data["pages"], author_id = data["author_id"])
         elif table_name == "author":
             ins = table.insert().values(name=data["name"], origin = data["origin"])
         elif table_name == "borrow":
             ins = table.insert().values(book_id=data["book_id"], status = data["status"], date = data["date"])    
+        
         with self.conn as conn:
             conn.execute(ins)
         return str(ins)
--- app/routes.py
+++ app/routes.py
@@ -7,17 +7,17 @@ def home():
 
 @app.route("/<table_name>", methods=["GET", "POST"])
 def table(table_name):
-    models_sqlite.table.create_connection()
     results = models_sqlite.table.get_table(table_name)
+    
     if table_name == "book":
         form = forms.BookForm()
     elif table_name == "author":
         form = forms.AuthorForm()
     elif table_name == "borrow":
         form = forms.BorrowForm()
+    
     if request.method == "POST":
         if form.validate_on_submit():
-            models_sqlite.table.create_connection()
             models_sqlite.table.add(table_name,form.data)
     return render_template(f"{table_name}s.html", results=results, form=form)
 
